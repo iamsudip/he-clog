bbe922dd797593a07bace9336b1ca1a56f2c7168
iamsudip <iamsudip@programmer.net>
2017-03-29 17:54:52 +0000

    he-clog: add debug mode

--- server/constants.py
+++ server/constants.py
@@ -1,3 +1,4 @@
 # Do not change here, instead create `machine_settings.py`
 # and override the constants
 DEV = False
+DEBUG = True
--- server/handler.py
+++ server/handler.py
@@ -4,9 +4,9 @@ from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
 
-from constants import DEV
+from constants import DEV, DEBUG
 try:
-    from machine_settings import DEV
+    from machine_settings import *
 except ImportError:
     pass
 
@@ -14,6 +14,7 @@ except ImportError:
 class CassandraLogServiceHandler(object):
     def __init__(self, *args, **kwargs):
         self._cluster = Cluster()
+        self._keyspace = 'testks' if DEV else 'submissions'
 
     def get_session(self):
         return self._cluster.connect()
@@ -22,7 +23,7 @@ class CassandraLogServiceHandler(object):
         self._cluster.shutdown()
 
     def clog(self, message):
-        if DEV: print "Received Message: ", message
+        if DEBUG: print "Received Message: ", message
         kwrags = json.loads(message)
         session = self.get_session()
         # Insert logic to insert into cassandra
