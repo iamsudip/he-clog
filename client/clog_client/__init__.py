7b139ce320c2b424f9b36dcba4ee90ff86a45b8a
iamsudip <iamsudip@programmer.net>
2017-04-18 14:38:52 +0530

    he-clog: add SubmissionState

--- client/clog_client/__init__.py
+++ client/clog_client/__init__.py
@@ -9,6 +9,39 @@ from thrift.protocol import TBinaryProtocol
 from tclog import CassandraLogService
 
 
+class SubmissionState(object):
+    # Submission first time created
+    SUBMISSION_CREATED = 10
+
+    # IN evaluate_code, just before async call to compile_code
+    PREPARING_MESSAGE_FOR_COMPILING = 20
+
+    # In copile_code, just before caling CompileClient
+    QUEUED_FOR_COMPILING = 30
+
+    # IN code checker, just before calling code_checker_server
+    IN_COMPILE_CODE_CHECKER = 40
+
+    # Received from code_checker_server, just before queueing to
+    # compile_callback_queue
+    OUT_COMPILE_CODE_CHECKER = 50
+
+    # Compile worker received the message
+    COMPILE_WORKER_RECEIVED = 60
+
+    # Run status bulk created in compile_callback_worker, message also contains
+    # run_ids list which got created
+    RUN_STATUS_CREATED = 70
+
+    # In run_code, just before calling RunClient
+    QUEUED_FOR_RUN = 80
+
+    # After all async call finised, just before leaving compile_callback_worker
+    OUT_COMPILE_WORKER = 90
+
+    RUN_WORKER_RECEIVED = 100
+
+
 class TraceLogClient(object):
     hostname = 'tracelog-cassandra.hackerearth.com'
     port = 9000
@@ -39,7 +72,10 @@ class TraceLogClient(object):
     def log(cls, submission_id, user_id, state, run_id=None, context=None):
         transport, client = cls.new_transport()
         if context and not isinstance(context, dict):
-            raise Exception("Context should be of <type 'dict<string:string>'")
+            raise Exception("Context should be of <type 'dict'")
+        else:
+            context = json.dumps(context)
+
         log_timestamp = datetime.datetime.now()
         kwargs = {
             'submission_id': submission_id,
